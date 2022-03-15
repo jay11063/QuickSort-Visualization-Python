c5b97bb165543dbb73980350a80d0837ff359739
Jaeyoon Lee <lee.jaeyoon.lee@gmail.com>
2022-03-14 22:04:17 -0400

    Upload constants.py

---
+++ constants.py
@@ -0,0 +1,16 @@
+WIDTH, HEIGHT = 1920, 1080
+# WIDTH, HEIGHT = 512,256
+
+color_number = 0
+# BLACK = COLOR_SET[color_number][0]
+# WHITE = COLOR_SET[color_number][1]
+# RED = COLOR_SET[color_number][2]
+
+# BLACK, WHITE, RED
+COLOR_SET = [
+    [(27, 26, 23), (230, 213, 184), (228, 88, 38)],
+    [(83, 53, 53), (230, 204, 169), (255, 109, 109)],
+    [(106, 44, 112), (249, 237, 105), (184, 59, 94)],
+    [(7, 104, 159), (162, 213, 242), (255, 126, 103)],
+    [(12, 5, 109), (242, 93, 156), (182, 26, 174)]
+]
