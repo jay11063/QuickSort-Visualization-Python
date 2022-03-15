ca561c08e3887a2d3aa74b8a708a032d24d4c5ac
Jaeyoon Lee <lee.jaeyoon.lee@gmail.com>
2022-03-14 22:03:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
 import pygame
 import sys
 import random
+from constants import *
 
-# WIDTH, HEIGHT = 1920,1080
-WIDTH, HEIGHT = 800, 400
-BLACK = (0, 0, 0)
-WHITE = (255,255,255)
-# GREEN = (0,255,0)
-GREEN = (100, 235, 66)
-RED = (255,0,0)
-# RED = (208, 49, 45)
 
 class Bars:
-    def __init__(self, high=100):
-        self.arr = random.sample(range(1,high+1), high)
+    def __init__(self, high):
+        self.arr = random.sample(range(1, high+1), high)
         self.high = high
 
     def reset_arr(self):
-        self.arr = random.sample(range(1,self.high+1), self.high)
+        # Shuffle
+        for i in range(self.high-1, 1, -1):
+            j = random.randint(0, i)
+            self.swap(i, j)
+            self.update(i, j)
+        # self.arr = random.sample(self.arr, self.high)
 
-    def draw(self, screen, i, j):
+    def quit(self):
+        pygame.quit()
+        sys.exit()
+
+    def draw(self, i, j):
         for idx, a in enumerate(self.arr):
-            w = int((WIDTH/self.high))
+            w = (WIDTH/self.high)
             h = int(a*(HEIGHT/self.high))
             # if idx==pivot:
             #     color = GREEN
-            if idx==i or idx==j:
-                color = RED
+            if idx == i or idx == j:
+                color = COLOR_SET[color_number][2]
             else:
-                color = WHITE
-            pygame.draw.rect(screen, color, (idx*w, HEIGHT-h, w, h))
-    
+                color = COLOR_SET[color_number][1]
+            pygame.draw.rect(screen, color, (int(idx*w), HEIGHT-h, int(w), h))
+
+    def update(self, i=-1, j=-1):
+        screen.fill(COLOR_SET[color_number][0])
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    change_color()
+                elif event.key == pygame.K_ESCAPE:
+                    self.quit()
+        self.draw(i, j)
+        pygame.display.update()
+        fps.tick(60)
+
     def swap(self, i, j):
         self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
 
@@ -48,32 +64,32 @@ class Bars:
             if(left > right):
                 self.swap(right, pivot)
             else:
-                self.swap(left,right)
-            screen.fill(BLACK)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
-                    pygame.quit()
-                    sys.exit()
-            self.draw(screen, left, right)
-            pygame.display.update()
-            fps.tick(60)
+                self.swap(left, right)
+            self.update(left, right)
         self.quickSort(start, right - 1)
         self.quickSort(right + 1, end)
 
 
+def change_color():
+    global color_number
+    color_number += 1
+    color_number %= len(COLOR_SET)
+
+
 def main():
-    bars = Bars(400)
+    bars = Bars(WIDTH//10)
 
     while True:
         bars.quickSort(0, bars.high - 1)
         # print(bars.arr)
-        screen.fill(BLACK)
+        screen.fill(COLOR_SET[color_number][0])
         for idx, a in enumerate(bars.arr):
             w = int((WIDTH/bars.high))
             h = int(a*(HEIGHT/bars.high))
-            pygame.draw.rect(screen, WHITE, (idx*w, HEIGHT-h, w, h))
+            pygame.draw.rect(
+                screen, COLOR_SET[color_number][1], (idx*w, HEIGHT-h, w, h))
         pygame.display.update()
-        pygame.time.delay(600)
+        # pygame.time.delay(600)
         bars.reset_arr()
 
 
@@ -81,5 +97,5 @@ if __name__ == "__main__":
     pygame.init()
     pygame.display.set_caption('Quick Sort')
     fps = pygame.time.Clock()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
     main()
